d9c22708778c0b06130abae7057ebc64cc17ee32
belomarkovicdev <belomarkovic1@gmail.com>
2025-03-22 00:12:21 +0100

    removed redis

--- backend/src/main/java/com/jb/petTracker/repository/DeviceTrackingSessionRepository.java
+++ backend/src/main/java/com/jb/petTracker/repository/DeviceTrackingSessionRepository.java
@@ -1,9 +1,7 @@
 package com.jb.petTracker.repository; 
 
-import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jb.petTracker.model.DeviceTrackingSession;
@@ -11,6 +9,4 @@ import com.jb.petTracker.model.DeviceTrackingSession;
 @Repository
 public interface DeviceTrackingSessionRepository extends MongoRepository<DeviceTrackingSession, String> {
     DeviceTrackingSession findByDeviceId(String id);
-    @Query("{'deviceId': ?0}")
-    List<DeviceTrackingSession> findAllByDeviceId(String deviceId);
 }
